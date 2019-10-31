dbfe91781b0bb5834b1bcf87fc9bd5947557b018
DarkShield666 <daigj>
2019-10-31 17:06:17 +0800

    fix

--- database_connect/mysql_cn.py
+++ database_connect/mysql_cn.py
@@ -5,7 +5,7 @@ import pymysql
 
 
 class MysqlCn:
-    def __init__(self, host, port, user, password, database=None):
+    def __init__(self, host: str, port: int, user: str, password: str, database=None):
         self.connection = pymysql.connect(
             host=host,
             port=port,
@@ -23,3 +23,18 @@ class MysqlCn:
             sql = "truncate %s" % i
             self.cur.execute(sql)
         self.connection.commit()
+
+    # 删除表中数据，目前仅支持kwargs仅为一个k,v 格式
+    def delete_data(self, tablename: list, **kwargs):
+        for i in tablename:
+            for k in kwargs:
+                sql = "delete from %s where %s = \'%s\'" % (i, k, kwargs[k])
+                print(sql)
+                self.cur.execute(sql)
+        self.connection.commit()
+
+    def query_data(self, tablename):
+        sql = "select * from rc_user"
+        self.cur.execute(sql)
+        for i in self.cur.fetchall():
+            print(i)
