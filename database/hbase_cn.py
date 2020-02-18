d35e12f9dd1670545d1d5f7615633f689222a279
DarkShield666 <daigj>
2020-02-18 14:05:25 +0800

    update

---
+++ database/hbase_cn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# python3
+
+# pip install hbase-thrift, 可以查看到安装路径
+# 去github下载 https://github.com/626626cdllp/infrastructure/tree/master/hbase
+# 替换python3 Hbase文件/usr/local/lib/python3.6/dist-packages/hbase/Hbase.py和ttypes.py
+# (参考学习) https://blog.csdn.net/luanpeng825485697/article/details/81048468
+
+from thrift.transport import TSocket, TTransport
+from thrift.protocol import TBinaryProtocol
+from hbase import Hbase
+
+
+class HbaseCn:
+    def __init__(self, host, port):
+        self.transport = TSocket.TSocket(host, port)
+        self.transport = TTransport.TFramedTransport(self.transport)
+        self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
+        self.Hbase_client = Hbase.Client(self.protocol)
+        self.transport.open()
+
+    def query_data(self, tableName, row, column):
+        data = self.Hbase_client.get(tableName, row, column)
+        return data[0].value
+
+    def hbase_close(self):
+        self.transport.close()
---
+++ database/mysql_cn.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pymysql
+
+
+class MysqlCn:
+    def __init__(self, host: str, port: int, user: str, password: str, database=None):
+        self.connection = pymysql.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cur = self.connection.cursor()
+
+    def close(self):
+        self.connection.close()
+
+    def truncate_tables(self, tablename: list):
+        for i in tablename:
+            sql = "truncate %s" % i
+            self.cur.execute(sql)
+        self.connection.commit()
+
+    # 删除表中数据，目前仅支持kwargs仅为一个k,v 格式
+    def delete_data(self, tablename: list, **kwargs):
+        for i in tablename:
+            for k in kwargs:
+                sql = "delete from %s where %s = \'%s\'" % (i, k, kwargs[k])
+                print(sql)
+                self.cur.execute(sql)
+        self.connection.commit()
+
+    def query_data(self, tablename):
+        sql = "select * from table_name "
+        self.cur.execute(sql)
+        for i in self.cur.fetchall():
+            print(i)
